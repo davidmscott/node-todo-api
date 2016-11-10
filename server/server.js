91758aada9b5f74cd04f2c9b777501f935d00f78
davidmscott <davescott@Daves-MBP.T-mobile.com>
2016-11-09 21:36:36 -0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -103,6 +103,18 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user) => {
+		return user.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user);
+		});
+	}).catch((error) => {
+		res.status(400).send();
+	});
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
